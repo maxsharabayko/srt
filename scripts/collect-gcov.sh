c1f0f7c84f8785d8af083dbecdc52ff899ed8d20
Maxim Sharabayko <maxlovic@gmail.com>
2019-10-28 15:54:32 +0100

    [build] Added SonarCloud integration

--- scripts/collect-gcov.sh
+++ scripts/collect-gcov.sh
@@ -5,3 +5,33 @@ for x in ./**/*.o; do
   echo "x: $x"
   gcov "$gcov_data_dir/$x"
 done
+
+
+### Generate and copy gcov files ###
+
+#export TARGET_DIR=
+#export OUTPUT_DIR=
+#
+#cd "$TARGET_DIR"
+##mkdir -p "$OUTPUT_DIR"
+#
+#CDIR=""
+#for x in **/*.cpp; do
+#  if [ "$CDIR" != "$TARGET_DIR/${x%/*}" ]; then
+#    CDIR="$TARGET_DIR/${x%/*}"
+#    cd $CDIR
+#    gcov -p *.cpp
+#
+#    SUBDIR="${x%/*}"
+#    PREFIX="#${SUBDIR/\//\#}"
+#
+#    for f in *.gcov; do
+#        if [[ $f == \#* ]] ;
+#        then
+#           cp $f "$OUTPUT_DIR/$f"
+#        else
+#           cp $f "$OUTPUT_DIR/$PREFIX#$f"
+#        fi
+#    done
+#  fi
+#done
