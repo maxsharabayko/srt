2cbbce0c7f9663b4188d1925649220cdf30bf958
Maxim Sharabayko <maxsharabayko@haivision.com>
2020-07-17 12:19:15 +0200

    [core] Fixing flight flag size restriction

--- srtcore/core.cpp
+++ srtcore/core.cpp
@@ -3971,7 +3971,7 @@ void CUDT::startConnect(const sockaddr_any& serv_addr, int32_t forced_isn)
     }
 
     m_ConnReq.m_iMSS            = m_iMSS;
-    m_ConnReq.m_iFlightFlagSize = (m_iRcvBufSize < m_iFlightFlagSize) ? m_iRcvBufSize : m_iFlightFlagSize;
+    m_ConnReq.m_iFlightFlagSize = min(m_iRcvBufSize, m_iFlightFlagSize);
     m_ConnReq.m_iID             = m_SocketID;
     CIPAddress::ntop(serv_addr, (m_ConnReq.m_piPeerIP));
 
