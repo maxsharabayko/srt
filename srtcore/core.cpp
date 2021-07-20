be9fe3d214f8d0d22cb1ad8192e288522ed90d25
Maxim Sharabayko <maxlovic@gmail.com>
2021-07-20 12:30:06 +0200

    Added readBuffer function

--- srtcore/core.cpp
+++ srtcore/core.cpp
@@ -6295,7 +6295,7 @@ int srt::CUDT::receiveBuffer(char *data, int len)
 
     enterCS(m_RcvBufferLock);
 #if ENABLE_NEW_RCVBUFFER
-    const int res = -1;
+    const int res = m_pRcvBuffer->readBuffer(data, len, m_iRcvLastAck);
 #else
     const int res = m_pRcvBuffer->readBuffer(data, len);
 #endif
